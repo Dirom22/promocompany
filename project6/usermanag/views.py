013f2b6303e3d19ed0b4a29fbd1eecf7e246f59a
Dirom22 <s2020dima@yandex.ru>
2023-03-01 10:56:30 +0300

    versia1

---
+++ project6/usermanag/views.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import login, authenticate
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.http import HttpRequest, HttpResponse
+from usermanag.forms import CustomUserCreationForm, LoginForm, ChangeUser
+from django.contrib.auth import get_user_model
+
+# Create your views here.
+
+user = get_user_model()
+
+def dashboard(request):
+	return render(request, 'dashboard.html')
+
+
+def register(request):
+	if request.method == 'GET':
+		return render(request, 'register.html', 
+			{'form': CustomUserCreationForm}
+			)
+	elif request.method == 'POST':
+		form = CustomUserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			return render(request, 'dashboard.html')
+		else:
+			data = form.errors
+			print(data)
+			return render(request, 'error.html', {'data': data})
+
+
+def error(request):
+	return render(request, 'error.html')
+
+
+def profile(request):
+	if request.method == 'POST':
+		form = ChangeUser(request.POST, instance=request.user)
+		if form.is_valid():
+			form.save()
+	form = ChangeUser()
+	#form.fields['first_name'].initial = request.user.first_name
+    #form.fields['last_name'].initial = request.user.last_name
+    #form.fields['phone_number'].initial = request.user.phone_number
+    #form.fields['email'].initial = request.user.email
+	data = {'form': form}
+	return render(request, 'profile.html', data)
